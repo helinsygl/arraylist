5446146d4feb098e876a47840b717a85c4c2d7f9
Helin <helinn.sygl@gmail.com>
2024-03-21 15:05:55 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+ArrayList<String> array=new ArrayList<String>();
+array.add("helin");
+array.add("eda");
+array.add("rabia");
+System.out.println(array);
+    }
+}
