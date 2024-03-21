b40917993f0a96dc8c6913d5af7bb633169a4d9a
Helin <helinn.sygl@gmail.com>
2024-03-21 15:07:20 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-ArrayList<String> array=new ArrayList<String>();
+ArrayList<String> array=new ArrayList<>();
 array.add("helin");
 array.add("eda");
 array.add("rabia");
